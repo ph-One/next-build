97aad3e931f7a0f39c29e4f44da33ae12b8f5e57
Tim Cosgrove <timcosgrove@users.noreply.github.com>
2023-09-11 13:55:03 -0700

    Add the footer and the div for the logout modal.

--- src/templates/globals/footer/index.tsx
+++ src/templates/globals/footer/index.tsx
@@ -1,10 +1,17 @@
 export const Footer = () => {
   return (
-    <section role="contentinfo">
-      <div id="announcement-root"></div>
-      <footer className="footer">
-        <div id="footerNav" data-testid="footer"></div>
-      </footer>
-    </section>
+    <>
+      <section role="contentinfo">
+        <div id="announcement-root"></div>
+        <footer className="footer">
+          <div id="footerNav" data-testid="footer"></div>
+        </footer>
+      </section>
+      <div id="logout-modal-root"></div>
+      {/*
+        “To care for him who shall have borne the battle and for his widow, and his orphan.”
+        - Abraham Lincoln
+      */}
+    </>
   )
 }
