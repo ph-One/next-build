77721645029e93fbf36dd86b58b48b1f92eea4ab
jtmst <josh.troy.mills@gmail.com>
2023-11-28 14:05:42 -0500

    config and a11y test setup

--- playwright.config.js
+++ playwright.config.js
@@ -2,8 +2,17 @@ const { devices } = require('@playwright/test')
 
 module.exports = {
   testDir: './playwright/tests',
+  outputDir: './playwright/test-results',
+  reporter: [['html', { outputFolder: './playwright/test-report' }]],
+  fullyParallel: true,
+  retries: 1,
+  screenshot: 'only-on-failure',
+  video: 'retain-on-failure',
+  viewport: { width: 1280, height: 720 },
+  timeout: 180000,
 
   use: {
     baseURL: process.env.SITE_URL || 'http://localhost:8001',
+    trace: 'on-first-retry',
   },
 }
